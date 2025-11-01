6d2f1336c3eef309e7a01a54a06e88ef77cb1386
QNhut29104 <nhutdn8388@gmail.com>
2025-11-01 16:50:37 +0700

    cấu hình kết nối

---
+++ aboutus.php
@@ -0,0 +1,7 @@
+<header>
+	<h1>Chào mừng đến với EShop</h1>
+	<p>Nơi cung cấp các sản phẩm điện tử chất lượng hàng đầu</p>
+</header>
+<div class="container">
+	<p>Khám phá bộ sưu tập các thiết bị điện tử hiện đại từ điện thoại, laptop đến phụ kiện thông minh. EShop cam kết mang đến sản phẩm chính hãng, giá cả cạnh tranh và dịch vụ tận tâm!</p>
+</div>
