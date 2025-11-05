e352894653802aa1936d855984bb15ce5442e164
QNhut29104 <nhutdn8388@gmail.com>
2025-11-05 16:02:47 +0700

    phần trang home

---
+++ home.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ESHOP</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+	<?php
+		if(isset($_GET['display_limit']))
+			$_SESSION['display_limit'] += 8;
+		else
+			$_SESSION['display_limit'] = 12;
+		$display_limit = $_SESSION['display_limit'];
+		
+		$sql = "SELECT `tbl_sanpham`.*, `tbl_loaisanpham`.`TenLoai`, `tbl_thuonghieu`.`TenThuongHieu`
+				FROM `tbl_sanpham` 
+					LEFT JOIN `tbl_loaisanpham` ON `tbl_sanpham`.`MaLoai` = `tbl_loaisanpham`.`MaLoai` 
+					LEFT JOIN `tbl_thuonghieu` ON `tbl_sanpham`.`MaTH` = `tbl_thuonghieu`.`MaTH`
+				ORDER BY LuotMua DESC, LuotXem DESC
+				LIMIT ".$display_limit;
+					
+		$danhsach = $connect->query($sql);
+		//Nếu kết quả kết nối không được thì xuất báo lỗi và thoát
+		if (!$danhsach) {
+			die("Không thể thực hiện câu lệnh SQL: " . $connect->error);
+			exit();
+		}
+		$sql = "SELECT `tbl_sanpham`.*, `tbl_loaisanpham`.`TenLoai`, `tbl_thuonghieu`.`TenThuongHieu`
+				FROM `tbl_sanpham` 
+					LEFT JOIN `tbl_loaisanpham` ON `tbl_sanpham`.`MaLoai` = `tbl_loaisanpham`.`MaLoai` 
+					LEFT JOIN `tbl_thuonghieu` ON `tbl_sanpham`.`MaTH` = `tbl_thuonghieu`.`MaTH`";
+		$danhsachall = $connect->query($sql);
+		$count_query = mysqli_num_rows($danhsachall);
+		
+		while($row = $danhsach->fetch_array(MYSQLI_ASSOC)){
+			$path = "images/".$row['TenLoai']."/".$row['TenThuongHieu']."/".$row['HinhAnh'];
+			if($row['TiLeGiam'] > 0)
+				$giaban = $row['GiaGoc'] - (($row['TiLeGiam'] /100) * $row['GiaGoc']);
+			else
+				$giaban = $row['GiaGoc'];
+		?>
+			<div class="khungsanpham">
+				<div class="card">					
+					<a href='index.php?do=sanpham_chitiet&MaSP=<?php echo $row['MaSP'];?>&MaTH=<?php echo $row['MaTH'];?>'>
+						<img class="hinhanhsp" src="<?php echo $path;?>" style='width: 200px; height: 150px;'>
+						<br />
+					</a>
+					<span class="luotxem"><?php echo $row['LuotXem'];?> lượt xem </span>
+					<span class="luotmua"><?php echo $row['LuotMua'];?> lượt mua </span><br/>
+					<?php
+					if($row['TiLeGiam'] > 0)
+						echo "<span class=\"giagoc\">". number_format($row['GiaGoc'])." đ</span>";
+					?>
+					</span><span class="giaban"><?php echo number_format($giaban);?> đ</span>
+				</div>
+				<p><a  class="tensp" href='index.php?do=sanpham_chitiet&MaSP=<?php echo $row['MaSP'];?>&MaTH=<?php echo $row['MaTH'];?>'><?php echo $row['TenSanPham'];?></a></p>
+			</div>
+		<?php
+		}
+		if($count_query > $_SESSION['display_limit'])
+		{
+			echo "<h3 class=\"xemthem\"><a href='index.php?do=home&display_limit=ok'>Xem thêm các sản phẩm khác</a></h3></td>";
+		}
+	?>
+</body>
+</html>
